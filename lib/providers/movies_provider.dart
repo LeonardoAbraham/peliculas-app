3c632814e80283aee4ffaf5974e9c3ddf9ed0351
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-02-20 16:16:14 -0600

    Peticiones HTTP

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,9 +1,16 @@
 
 
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier{
+
+    String _apiKey      = 'b4b44333678b34388f28450ef0799dbc';
+    String _baseUrl     = 'api.themoviedb.org';
+    String _languaje    = 'es-ES';
 
     MoviesProvider(){
         print('MoviesProvider inicializado');
@@ -11,7 +18,18 @@ class MoviesProvider extends ChangeNotifier{
     }
 
     getOnDisplayMovies() async {
-        print('getOnDisplayMovies');
+        var url =
+        Uri.https(_baseUrl, '3/movie/now_playing', {
+            'api_key': _apiKey,
+            'languaje': _languaje,
+            'page': '1'
+        });
+
+        // Await the http get response, then decode the json-formatted response.
+        final response = await http.get(url);
+        final Map<String, dynamic> decodeData = json.decode(response.body);
+
+        print(decodeData);
     }
 
 }
