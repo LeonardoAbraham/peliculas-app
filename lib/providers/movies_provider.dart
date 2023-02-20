0d380cbb81887cd54c3e6419d554b22b97ef05bf
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-02-20 15:43:27 -0600

    provider y paquete http

---
+++ lib/providers/movies_provider.dart
@@ -0,0 +1,17 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class MoviesProvider extends ChangeNotifier{
+
+    MoviesProvider(){
+        print('MoviesProvider inicializado');
+        this.getOnDisplayMovies();
+    }
+
+    getOnDisplayMovies() async {
+        print('getOnDisplayMovies');
+    }
+
+}
